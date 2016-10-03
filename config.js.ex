6de92b23c5630345c7c5d7f7197badfa962f23a5
Jessica Lawshe <jessica@jlawshe.com>
2016-10-03 12:25:29 -0500

    Config file example used to connect to database
    replace JOURNALNAME with journal name

---
+++ config.js.ex
@@ -0,0 +1,34 @@
+var config = {
+    journalSettings: {
+    	JOURNALNAME: {
+    		issn: "",
+    		name: "",
+    		shortName: "",
+    		dbUrl: "",
+    		doi : "",
+    		mongo : {
+    			server : "",
+    			port : "",
+    			name : "",
+    			user : "",
+    			password : ""
+    		}
+    	}
+    },
+    journalSettingsDev: {
+        JOURNALNAME: {
+            issn: "",
+            name: "",
+            shortName: "",
+            dbUrl: "",
+            doi : "",
+            mongo : {
+                server : "",
+                port : "",
+                name : "",
+                user : "",
+                password : ""
+            }
+        }
+    }
+};
